9601db9e640fd8ab01bb2c4e8b40e9940b537337
nathanmbicho <nathanmbicho@gmail.com>
2025-02-03 13:14:47 +0300

    Assignment 3 - Firebase authentication and authorization, Routing with react-router, and date-fns and Firestore server timestamps to add a wait time to each ticket
    - fixing redirection

--- assignment3/next.config.ts
+++ assignment3/next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/login',
+                permanent: true,
+            },
+        ];
+    },
   /* config options here */
     images: {
         remotePatterns: [{
